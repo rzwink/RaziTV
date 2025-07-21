3370be116250f99ff304e4758c33fdaec1a2abe0
Rob Z <rzwink@gmail.com>
2025-07-21 13:06:33 -0400

    add wifi-connect

--- src/modules/wifi-config-web/filesystem/usr/local/bin/launch_browser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-UNIQUE_ID=$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2)
-URL="https://razititle.com/activate/$UNIQUE_ID"
-
-/usr/bin/chromium-browser --kiosk --noerrdialogs --disable-infobars "$URL"
--- src/modules/wifi-config-web/filesystem/usr/local/bin/start_hotspot_if_needed.sh
+++ src/modules/wifi-config-web/filesystem/usr/local/bin/start_hotspot_if_needed.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+exec > /tmp/wifi-launch.log 2>&1
+set -x
+
+# Unblock and prepare WiFi interface
+rfkill unblock wifi
+ip link set wlan0 up
 
 SSID=$(iwgetid -r)
 
 if [ -z "$SSID" ]; then
     echo "No WiFi detected. Starting hotspot..."
 
-    # Start AP services
     systemctl start hostapd
     systemctl start dnsmasq
     systemctl start lighttpd
 
-    # Optional: open local help page in kiosk mode if HDMI is attached
-    if [ -n "$(tvservice -s | grep '0x')" ]; then
-        /usr/bin/chromium-browser --kiosk --noerrdialogs --disable-infobars "http://192.168.4.1/help.html"
-
+    # Show help page on screen if display is attached
+    if tvservice -s | grep -q 'HDMI'; then
+        sleep 2  # give lighttpd time to start
+        /usr/bin/chromium-browser --kiosk --noerrdialogs --disable-infobars "http://192.168.4.1/help.html" &
     fi
 else
     echo "WiFi detected ($SSID). Stopping hotspot and launching kiosk..."
@@ -22,5 +27,9 @@ else
     systemctl stop dnsmasq
     systemctl stop lighttpd
 
-    /usr/local/bin/launch_browser.sh
+    UNIQUE_ID=$(awk '/Serial/ {print $3}' /proc/cpuinfo)
+    URL="https://razititle.com/activate/$UNIQUE_ID"
+
+    sleep 2  # give time for system to stabilize after network switch
+    /usr/bin/chromium-browser --kiosk --noerrdialogs --disable-infobars "$URL" &
 fi
